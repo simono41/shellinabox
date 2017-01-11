456303fce5916ccede022295df2da3eb58e55ee9
Simon Rieger <simonrieger1999@gmail.com>
2017-01-11 13:44:13 +0100

    auto script

---
+++ auto.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+  echo "Als root Angemeldet"
+  
+apt-get install git libssl-dev libpam0g-dev zlib1g-dev dh-autoreconf
+
+cd /opt/
+
+git clone https://github.com/shellinabox/shellinabox.git && cd shellinabox
+
+autoreconf -i
+
+./configure && make
+
+cp shellinabox.service /etc/systemd/system/
+
+systemctl daemon-reload
+
+systemctl enable shellinabox.service
+
+# adduser
+
+echo adduser webssh
+
+useradd webssh
+
+mkdir /home/webssh
+
+chmod 770 -R /home/webssh
+
+chown -R webssh /home/webssh
+
+passwd "webssh <<EOT
+webssh
+webssh
+EOT
+
+cp  shellinabox_sshwrapper.sh /home/webssh/
+
+systemctl start shellinabox.service
